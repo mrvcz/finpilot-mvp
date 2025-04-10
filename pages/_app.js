02162505c24dbe39340fb3ca50b74cbafad630a3
Dominik Pozniak <domiprzemhq@gmail.com>
2025-04-11 00:48:07 +0200

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+// pages/_app.js
+import '../styles/globals.css';
+import { AuthProvider } from '../context/AuthContext';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
